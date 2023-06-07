c89dffc3c4f5627d3e42d702d9993e31f3820d9f
Ralfs <ralfsssenfelds@gmail.com>
2023-06-07 10:32:07 +0300

    Saprotamāk izvēlēti mainīgo nosaukumi, galvenās programmas rindiņas paskaidrotas, prezentācijā ievietoti izmantotie mācību materiāli

--- Tests/src/Tests.java
+++ Tests/src/Tests.java
@@ -5,35 +5,35 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 
 public class Tests {
-  private static String[] jautajumi;
-  private static String[][] atbildes;
-  private static int[][] pareizasAtbildes;
-
+  private static String[] jautajumi;	//Masīvs, kas uzglabā jautājumus
+  private static String[][] atbildes;	//Masīvs, kas uzglabā atbildes
+  private static int[][] parVaiNepAtbildes;	//Masīvs, kas satur pareižo atbilžu vērtības
+  //Galvenā metode
   public static void main(String[] args) {
-    Object[] opcijas = { "Sākt", "Iziet" };
-    String message = "<html><div style='text-align: center;'>Sveiks, izvēlies \"Sākt\", lai pildītu testu par<br>"
+    Object[] opcijas = { "Sākt", "Iziet" }; //Masīvs, kas uzglabā vērtības, lai izietu vai sāktu testu
+    //Centrēts teksts, ko lietotājam parāda programmas sākumā, teksts centrēts izmantojot html funkcijas
+    String teksts = "<html><div style='text-align: center;'>Sveiks, izvēlies \"Sākt\", lai pildītu testu par<br>"
         + "viendimensiju masīviem, \"Iziet\", lai aizvērtu aplikāciju.</div></html>";
-
-    int izvele = JOptionPane.showOptionDialog(null, message, "Sākums",
+    //Izvēles logs. kurā ir iespējams sākt testu vai iziet
+    int izvele = JOptionPane.showOptionDialog(null, teksts, "Sākums",
         JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcijas, opcijas[0]);
-
+    //switch, lai lietotājs varētu izvēlēties turpmāko darbību
     switch (izvele) {
-      case 0:
-        saktTestu();
+      case 0:	
+        saktTestu();	//Aizsūta lietotāju uz testa pildīšanas daļu
         break;
       case 1:
-        JOptionPane.showMessageDialog(null, "Aplikācija apstādināta.");
+        JOptionPane.showMessageDialog(null, "Aplikācija apstādināta.");	//Izvada ziņu par spēles aizvēršanu
         break;
       default:
-        JOptionPane.showMessageDialog(null, "Izvēle neeksistē!");
+        JOptionPane.showMessageDialog(null, "Aplikācija apstādināta.");	//Izvada ziņu par spēles aizvēršanu
     }
   }
-
+  //Metode, kas satur 
   public static void saktTestu() {
-    // Initialize the answerCheckboxes array
-    // Jautājumi
+    // Jautājumi uz kuriem lietotājam jāatbild
     jautajumi = new String[] {
-        "Kas ir masīvs?",
+        "Kas ir masīvs?",																	
         "Kā deklarēt masīvu Java programmēšanas valodā?",
         "Kā piekļūt masīva elementam?",
         "Kas ir masīva garums?",
@@ -44,63 +44,63 @@ public class Tests {
         "Kā pārlūkot visus masīva elementus?",
         "Kas notiek, ja mēģina piekļūt masīva elementam ar indeksu, kas ir ārpus robežām?"
     };
-
-    JCheckBox[][] answerCheckboxes = new JCheckBox[jautajumi.length][4]; // Initialize the answerCheckboxes array
-
+    // Masīvs, kas satur JCheckBoxes
+    JCheckBox[][] atbildesLodzini = new JCheckBox[jautajumi.length][4]; 
     // Atbildes uz jautājumiem
     atbildes = new String[][] {
-        { "Mainīgais, kas var uzglabāt vairākus vienāda tipa vērtības.", // A
-            "Datu struktūra, kas var uzglabāt gan primārās, gan atsauces tipus.", // B
-            "Objektu kolekcija, kam var dinamiski mainīt izmērus.", // C
-            "Atslēgvārds, ko izmanto mainīgo deklarēšanai" }, // D
-
-        { "int[] mansMasīvs;", // A
-            "mansMasīvs = new int[];", // B
-            "int mansMasīvs[];", // C
-            "mansMasīvs = new int[];" }, // D
-
-        { "Izmantojot elementa indeksu kvadrātiekavās.", // A
-            "Izsaucot metodi masīva objektā.", // B
-            "Izmantojot elementa vērtību.", // C
-            "Nevar piekļūt masīva elementam." }, // D
-
-        { "Pēdējā elementa indekss masīvā.", // A
-            "Elementu skaits masīvā.", // B
-            "Atmiņas izmērs, kas piešķirts masīvam.", // C
-            "Maksimālā vērtība, kas var tikt uzglabāta masīvā." }, // D
-
-        { "int[] mansMasīvs = {1, 2, 3};", // A
-            "int mansMasīvs[] = new int[3];", // B
-            "mansMasīvs = {1, 2, 3};", // C
-            "int mansMasīvs[] = {1, 2, 3};" }, // D
-
-        { "Nē, visiem masīva elementiem jābūt vienāda tipa.", // A
-            "Jā, Java ļauj sajaukt dažāda tipa elementus masīvā.", // B
-            "Tikai tad, ja masīvs ir deklarēts kā ģeneriskais tips.", // C
-            "Tikai tad, ja masīvs ir deklarēts kā objekta tips." }, // D
-
-        { "Izmantojot iebūvēto max() metodi klases Array.", // A
-            "Sakārtojot masīvu augošā secībā.", // B
-            "Izmantojot Math.max() funkciju.", // C
-            "Izmantojot ciklu, lai salīdzinātu katru elementu ar pašreizējo maksimālo." }, // D
-
-        { "Jā, var dinamiski pievienot vai noņemt elementus.", // A
-            "Nē, masīva izmērs ir fiksēts pēc deklarēšanas.", // B
-            "Tikai tad, ja izmanto ArrayList klasi, nevis parasto masīvu.", // C
-            "Tikai tad, ja izmantojat resize() metodi, lai mainītu masīva izmēru." }, // D
-
-        { "Izmantojot for ciklu ar indeksa mainīgo.", // A
-            "Izmantojot forEach ciklu.", // B
-            "Izmantojot while ciklu un palielinot skaitītāja mainīgo.", // C
-            "Izsaucot iterate() metodi klases Array." }, // D
-
-        { "Programma izmet ArrayIndexOutOfBoundsException kļūdu.", // A
-            "Indeksēšana apvīžas un piekļūst citam elementam.", // B
-            "Kompilators izmet brīdinājumu, bet programma normāli darbojas.", // C
-            "Programma izmet kļūdu un aptur darbību." } // D
+    	// 1. jautājums
+        { "Mainīgais, kas var uzglabāt vairākus vienāda tipa vērtības.", 								// A
+            "Datu struktūra, kas var uzglabāt gan primārās, gan atsauces tipus.", 						// B
+            "Objektu kolekcija, kam var dinamiski mainīt izmērus.",										// C
+            "Atslēgvārds, ko izmanto mainīgo deklarēšanai" },											// D
+        // 2. jautājums
+        { "int[] mansMasīvs;", 																			// A
+            "mansMasīvs = new int[];", 																	// B
+            "int mansMasīvs[];", 																		// C
+            "mansMasīvs = new int[];" }, 																// D
+        // 3. jautājums
+        { "Izmantojot elementa indeksu kvadrātiekavās.", 												// A
+            "Izsaucot metodi masīva objektā.", 															// B
+            "Izmantojot elementa vērtību.", 															// C
+            "Nevar piekļūt masīva elementam." }, 														// D
+        // 4. jautājums
+        { "Pēdējā elementa indekss masīvā.", 															// A
+            "Elementu skaits masīvā.", 																	// B
+            "Atmiņas izmērs, kas piešķirts masīvam.", 													// C
+            "Maksimālā vērtība, kas var tikt uzglabāta masīvā." }, 										// D
+        // 5. jautājums
+        { "int[] mansMasīvs = {1, 2, 3};", 																// A
+            "int mansMasīvs[] = new int[3];", 															// B
+            "mansMasīvs = {1, 2, 3};", 																	// C
+            "int mansMasīvs[] = {1, 2, 3};" }, 															// D
+        // 6. jautājums
+        { "Nē, visiem masīva elementiem jābūt vienāda tipa.", 											// A
+            "Jā, Java ļauj sajaukt dažāda tipa elementus masīvā.", 										// B
+            "Tikai tad, ja masīvs ir deklarēts kā ģeneriskais tips.", 									// C
+            "Tikai tad, ja masīvs ir deklarēts kā objekta tips." }, 									// D
+        // 7. jautājums
+        { "Izmantojot iebūvēto max() metodi klases Array.", 											// A
+            "Sakārtojot masīvu augošā secībā.", 														// B
+            "Izmantojot Math.max() funkciju.", 															// C
+            "Izmantojot ciklu, lai salīdzinātu katru elementu ar pašreizējo maksimālo." }, 				// D
+        // 8. jautājums
+        { "Jā, var dinamiski pievienot vai noņemt elementus.",											// A
+            "Nē, masīva izmērs ir fiksēts pēc deklarēšanas.", 											// B
+            "Tikai tad, ja izmanto ArrayList klasi, nevis parasto masīvu.",								// C
+            "Tikai tad, ja izmantojat resize() metodi, lai mainītu masīva izmēru." }, 					// D
+        // 9. jautājums
+        { "Izmantojot for ciklu ar indeksa mainīgo.", 													// A
+            "Izmantojot forEach ciklu.", 																// B
+            "Izmantojot while ciklu un palielinot skaitītāja mainīgo.", 								// C
+            "Izsaucot iterate() metodi klases Array." }, 												// D
+        // 10. jautājums
+        { "Programma izmet ArrayIndexOutOfBoundsException kļūdu.", 										// A
+            "Indeksēšana apvīžas un piekļūst citam elementam.", 										// B
+            "Kompilators izmet brīdinājumu, bet programma normāli darbojas.", 							// C
+            "Programma izmet kļūdu un aptur darbību." } 												// D
     };
-    // Pareizo atbilžu indeksi
-    pareizasAtbildes = new int[][] {
+    //Masīvs, kas satur pareizo atbilžu indeksus
+    parVaiNepAtbildes = new int[][] {
         { 0, 1 },
         { 0, 2 },
         { 0, 3 },
@@ -112,88 +112,122 @@ public class Tests {
         { 0, 2 },
         { 0, 3 }
     };
-
-    boolean[] pareizas = new boolean[jautajumi.length];
+    // izveido interaktīvu logu, kurā tiek rādīti jautājumi un atbildes izvēles iespējas.
+    // Izveido boolean masīvu "parVaiNep", kurā tiks glabāta informācija par to, vai katrs jautājums ir atbildēts pareizi vai nepareizi.
+    boolean[] parVaiNep = new boolean[jautajumi.length];
+    // Definē mainīgo "numJautajumi", kas norāda uz kopējo jautājumu skaitu.
     int numJautajumi = jautajumi.length;
-
+    // Izveido JPanel ar 5 rindiņām un 1 kolonnas izkārtojumu.
     for (int i = 0; i < numJautajumi; i++) {
       JPanel panel = new JPanel(new GridLayout(5, 1));
-
       JLabel questionLabel = new JLabel(jautajumi[i]);
+      // Izveido JLabel ar konkrēto jautājumu un pievieno to panelim.
       panel.add(questionLabel);
-
       for (int j = 0; j < 4; j++) {
-        answerCheckboxes[i][j] = new JCheckBox(atbildes[i][j]);
-        panel.add(answerCheckboxes[i][j]);
+        atbildesLodzini[i][j] = new JCheckBox(atbildes[i][j]);
+        // Izveido JCheckBox komponentes ar atbildēm un pievieno tās panelim.
+        panel.add(atbildesLodzini[i][j]);
       }
-
+      // Parāda JOptionPane dialogu ar izveidoto paneli, kurā tiek attēlots jautājums un atbildes izvēles iespējas.
+      // Lietotājs var noklikšķināt uz "OK" vai "Cancel" pogām.
       int izvele = JOptionPane.showConfirmDialog(null, panel, "Jautājums " + (i + 1),
           JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
       if (izvele == JOptionPane.OK_OPTION) {
+    	// Izveido boolean masīvu "izveles" ar 4 elementiem, kas glabās informāciju par lietotāja izvēlētajām atbildēm.
         boolean[] izveles = new boolean[4];
         for (int j = 0; j < 4; j++) {
-          izveles[j] = answerCheckboxes[i][j].isSelected();
+          // Saglabā lietotāja izvēlētās atbildes statusu (izvēlētas vai neizvēlētas) masīvā "izveles".
+          izveles[j] = atbildesLodzini[i][j].isSelected();
         }
-
-        pareizas[i] = parbauditAtbildes(i, izveles);
+        // Izsauc funkciju "parbauditAtbildes", lai pārbaudītu, vai izvēlētās atbildes ir pareizas vai nepareizas, 
+        // un rezultātu saglabā masīvā "parVaiNep".
+        parVaiNep[i] = parbauditAtbildes(i, izveles);
       } else {
+    	// Ja lietotājs ir atcēlis atbildi vai aizvēris logu, iestata "numJautajumi" vērtību uz sākuma vērtību
         numJautajumi = i;
         break;
       }
     }
-
-    paraditRezultatus(pareizas);
+    // Izvada lietotajam testa rezultātus
+    paraditRezultatus(parVaiNep);
   }
 
-  // Metode, kas pārbauda, vai lietotāja izvēlētie atbilžu varianti sakrīt ar
-  // pareizajiem indeksiem
+  //Metode, kas pārbauda, vai lietotāja izvēlētie atbilžu varianti sakrīt ar pareizajiem indeksiem
   public static boolean parbauditAtbildes(int jautajumaIndeks, boolean[] izveles) {
-    int[] pareizasIndeksi = pareizasAtbildes[jautajumaIndeks];
+	  int[] parVaiNepIndeksi = parVaiNepAtbildes[jautajumaIndeks];
+	  // Iegūst pareizos un nepareizos indeksus no masīva "parVaiNepAtbildes", 
+	  // kas atbilst jautājumam ar indeksu "jautajumaIndeks".
 
-    for (int i = 0; i < 4; i++) {
-      boolean irPareizi = satur(pareizasIndeksi, i);
-      boolean irIzvelets = izveles[i];
+	  for (int i = 0; i < 4; i++) {
+		  boolean irPareizi = satur(parVaiNepIndeksi, i);
+		  // Pārbauda, vai indekss "i" ir sastopams masīvā "parVaiNepIndeksi" 
+		  // un atgriež, vai tas ir pareizs vai nepareizs atbilžu variants.
 
-      if (irPareizi != irIzvelets) {
-        return false;
-      }
-    }
+		  boolean irIzvelets = izveles[i];
+		  // Iegūst informāciju par to, vai lietotājs ir izvēlējies atbildi ar indeksu "i".
 
-    return true;
+		  if (irPareizi != irIzvelets) {
+			  // Ja pareizs/nepareizs indekss nesakrīt ar lietotāja izvēlēto atbildi,
+			  // tad atgriežam "false", norādot, ka atbilde ir nepareiza.
+			  return false;
+		  }
+	  }
+
+	  // Ja visi pareizie/nepareizie indeksi sakrīt ar lietotāja izvēlētajām atbildēm,
+	  // tad atgrieža "true", norādot, ka atbilde ir pareiza.
+	  return true;
   }
-
-  // Metode, kas parada rezultātus pēc testa pabeigšanas
-  public static void paraditRezultatus(boolean[] pareizas) {
-    StringBuilder rezultatuVeidotajs = new StringBuilder();
-    rezultatuVeidotajs.append("Testa rezultāti:\n\n");
-
-    int numJautajumi = jautajumi.length;
-    int numPareizi = 0;
-
-    for (int i = 0; i < numJautajumi; i++) {
-      char atzime = pareizas[i] ? '\u2713' : '\u2717';
-      rezultatuVeidotajs.append("Jautājums ").append(i + 1).append(": ").append(atzime).append("\n");
-
-      if (pareizas[i]) {
-        numPareizi++;
-      }
-    }
-
-    double procents = (double) numPareizi / numJautajumi * 100;
-    rezultatuVeidotajs.append("\nKopā pareizi: ").append(numPareizi).append(" / ").append(numJautajumi);
-    rezultatuVeidotajs.append(" (").append(String.format("%.2f", procents)).append("%)");
-
-    JOptionPane.showMessageDialog(null, rezultatuVeidotajs.toString(), "Rezultāti", JOptionPane.PLAIN_MESSAGE);
-  }
-
-  // Metode, kas pārbauda, vai masīvs satur norādīto vērtību
+  
+  //Metode, kas pārbauda, vai masīvs satur norādīto vērtību
   public static boolean satur(int[] masivs, int vertiba) {
-    for (int i = 0; i < masivs.length; i++) {
-      if (masivs[i] == vertiba) {
-        return true;
-      }
-    }
-    return false;
+	  for (int i = 0; i < masivs.length; i++) {
+		  if (masivs[i] == vertiba) {
+			  return true;
+		  }
+	  }
+	  // Iterē caur masīvu "masivs" un pārbauda, vai kāda vērtība sakrīt ar norādīto vērtību "vertiba".
+	  // Ja sakrītība ir atrasta, tad atgriežam "true".
+
+	  return false;
+	  // Ja pēc masīva iterācijas nav atrasta sakrītība, atgriežam "false".
   }
+
+  
+  //Metode, kas parāda rezultātus pēc testa pabeigšanas
+  public static void paraditRezultatus(boolean[] parVaiNep) {
+	  StringBuilder rezultatuVeidotajs = new StringBuilder();
+	  // Izveido StringBuilder objektu, kurā tiks veidots rezultāta teksts.
+
+	  rezultatuVeidotajs.append("Testa rezultāti:\n\n");
+	  // Pievieno sākuma tekstu rezultāta veidotājam.
+
+	  int numJautajumi = jautajumi.length;
+	  int numPareizi = 0;
+	  // Definē mainīgos "numJautajumi" (jautājumu skaits) un "numPareizi" (pareizo atbilžu skaits) un inicializē tos.
+
+	  for (int i = 0; i < numJautajumi; i++) {
+		  char atzime = parVaiNep[i] ? '\u2713' : '\u2717';
+		  // Iegūst atzīmi (simbolu) - '✓' vai '✗' - atkarībā no tā, vai atbilde ir pareiza vai nepareiza, 
+		  // izmantojot operātoru "?".
+
+		  rezultatuVeidotajs.append("Jautājums ").append(i + 1).append(": ").append(atzime).append("\n");
+		  // Pievieno rezultāta tekstu veidotājam, norādot jautājuma numuru un atzīmi (pareizi vai nepareizi).
+
+		  if (parVaiNep[i]) {
+			  numPareizi++;
+			  // Ja atbilde ir pareiza, palielina "numPareizi" vērtību par 1.
+		  }
+	  }
+
+	  double procents = (double) numPareizi / numJautajumi * 100;
+	  // Aprēķina pareizo atbilžu procentuālo daudzumu.
+
+	  rezultatuVeidotajs.append("\nKopā pareizi: ").append(numPareizi).append(" / ").append(numJautajumi);
+	  rezultatuVeidotajs.append(" (").append(String.format("%.2f", procents)).append("%)");
+	  // Pievieno rezultātu tekstu veidotājam, norādot kopējo pareizo atbilžu skaitu un to procentuālo daudzumu.
+
+	  JOptionPane.showMessageDialog(null, rezultatuVeidotajs.toString(), "Rezultāti", JOptionPane.PLAIN_MESSAGE);
+	  // Rāda rezultātu dialoglodziņu ar veidoto rezultātu tekstu.
+  	}
 }
